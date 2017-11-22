1a840909e6feff3962381c596e730699aec7d4e1
bluznierca1 <grzegorz.lasak@grzegorzlasak.com>
2017-11-22 20:30:54 +0100

    Specifying footer/header/navbar for viewers

---
+++ public/layouts/header/restaurant_header_menu.php
@@ -0,0 +1,21 @@
+<?php include("restaurant_header.php"); ?>
+<!-- header section -->
+	<header role="banner" id="header">
+		<div class="row">
+
+			<!-- paralax main photo -->
+				<div class="parallax-container" style="height: 60vh;">
+
+					<div class="row">
+					<!-- dividing header into two sections: logo and menu -->
+						<div class="col s2 center-align">
+							<span><a href="../../index.php"><img src="../images/logo/logo.png" class="logo"></a></span>
+						</div>
+					<?php include("../layouts/navbar/restaurant_navbar.php"); ?>
+
+					<div class="parallax">
+						<img src="../images/home/home_photo.jpg">
+					</div>
+				</div>
+		</div>
+	</header>
---
+++ public/layouts/header/user_header_menu.php
@@ -0,0 +1,23 @@
+<?php include("user_header.php"); ?>
+<!-- header section -->
+	<header role="banner" id="header">
+		<div class="row">
+
+			<!-- paralax main photo -->
+				<div class="parallax-container">
+
+					<div class="row">
+					<!-- dividing header into two sections: logo and menu -->
+						<div class="col s2 center-align">
+							<span><img src="../images/logo/logo.png" class="logo"></span>
+						</div>
+					<?php include("../layouts/navbar/user_navbar.php"); ?>
+
+					<div class="parallax">
+						<img src="../images/home/home_photo.jpg">
+					</div>
+				</div>
+		</div>
+	</header>
+
+	
