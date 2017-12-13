783aaea84bf8f7630b0b70847c023b2102046dfe
bluznierca1 <grzegorz.lasak@grzegorzlasak.com>
2017-12-13 22:21:52 +0100

    File that initializes all of the classes in chosen order

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,6 +1,17 @@
 <!-- This file is for gathering all classes and files that are being included in other files.
 just for spacing space and making it easier -->
 
+<?php 
+
+	defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+
+	defined('SITE_ROOT') ? null : define("SITE_ROOT",
+		'C:'.DS.'laragon'.DS.'www'.DS.'getTable' .DS. 'GetTable');
+
+	defined('LIB_PATH') ? null : define("LIB_PATH", SITE_ROOT.DS.'includes');
+
+?>
+
 <?php require_once("config.php"); ?>
 <?php require_once("functions.php"); ?>
 
@@ -17,6 +28,7 @@ just for spacing space and making it easier -->
 	<!-- Functionality is to keep functions loaded before any classes tasks -->
 <?php require_once("validation_functions.php"); ?>
 
+<?php require_once("logo.php"); ?>
 <?php require_once("reservation.php"); ?>
 <?php require_once("pagination.php"); ?>
 <?php require_once("table.php"); ?>
