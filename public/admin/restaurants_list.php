0856e3427fa9e257bd0443f5e092d1156563b76b
bluznierca1 <grzegorz.lasak@grzegorzlasak.com>
2017-12-18 00:27:49 +0100

    List of restaurants - all of them / existing / removed

--- public/admin/restaurants_list.php
+++ public/admin/restaurants_list.php
@@ -14,13 +14,15 @@
   
   $admin = Admin::find_by_id($_SESSION['admin_id']);
   $restaurants = Restaurant::restaurants_marked();
+
+  $existing_restaurants = Restaurant::find_by_sql("SELECT * FROM restaurants WHERE existing = 'yes' ")
 ?>
 
 <?php include("../layouts/header/admin_header_menu.php"); ?>
 
 	<div class="row">    
  		<div class="col s12 m10 offset-m1">
-    	<h1 class="teal-text darken-2 center-align font-h1">Marked</h1>
+    	<h1 class="teal-text darken-2 center-align font-h1">Existing</h1>
       <p><?php echo display_message_errors($message_admin); ?></p>
       <table class="responsive-table bordered">
         <thead>
@@ -40,7 +42,7 @@
           </tr>
         </thead>
         <tbody>
- <?php foreach( $restaurants as $restaurant ) { ?>
+ <?php foreach( $existing_restaurants as $restaurant ) { ?>
           <tr>
           	<td><?php echo htmlentities($restaurant->restaurant_id); ?></td>
             <td><?php echo htmlentities($restaurant->username); ?></td>
@@ -85,6 +87,51 @@
         <tbody>
         <?php 
           $restaurants_to_mark = Restaurant::restaurants_to_mark();
+        ?>
+        <?php foreach( $restaurants_to_mark as $mark ) { ?>
+          <tr>
+            <td><?php echo htmlentities($mark->restaurant_id); ?></td>
+            <td><?php echo htmlentities($mark->username); ?></td>
+            <td><?php echo htmlentities($mark->created); ?></td>
+            <td><?php echo htmlentities($mark->name); ?></td>
+            <td><?php echo htmlentities($mark->email); ?></td>
+            <td><?php echo htmlentities($mark->city); ?></td>
+            <td><?php echo htmlentities($mark->street); ?></td>
+            <td><?php echo htmlentities($mark->number); ?></td>
+            <td><?php echo htmlentities($mark->zip_code); ?></td>
+            <td><?php echo htmlentities($mark->phone_number); ?></td>
+            <td><a href="mark_restaurant.php?restaurant_id=<?php echo htmlspecialchars($mark->restaurant_id); ?>" style="color: green;"><i class="material-icons">assignment_turned_in</i></a></td>
+            <td><a href="remove_restaurant.php?restaurant_id=<?php echo htmlspecialchars($mark->restaurant_id); ?>" style="color: red;"><i class="material-icons">remove</i></a></td>
+          </tr>
+  <?php } ?>
+        </tbody>
+      </table> 
+     </div>
+   </div>
+
+    <div class="row">
+    <div class="col s12 m10 offset-m1">
+      <h2 class="teal-text darken-2 center-align font-h1">All</h2>
+      <table class="responsive-table bordered">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Created</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>City</th>
+              <th>Street</th>
+              <th>Number</th>
+              <th>Zip Code</th>
+              <th>Phone number</th>
+              <th>Mark</th>
+              <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php 
+          $restaurants_to_mark = Restaurant::find_all();
         ?>
         <?php foreach( $restaurants_to_mark as $mark ) { ?>
           <tr>
