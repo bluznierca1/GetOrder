05a5e5738ddc67a2d4426dfa8e2993bc3be424b5
bluznierca1 <grzegorz.lasak@grzegorzlasak.com>
2017-12-18 00:26:59 +0100

    Removing restaurant option by admin

--- public/admin/remove_restaurant.php
+++ public/admin/remove_restaurant.php
@@ -5,7 +5,7 @@
 	if ( !$session_admin->is_logged_in() ){
 		redirect_to("login.php");
 	} else if( $session_user->is_logged_in() ){
-		$session_user->message(= "You are logged in as User. Can not go to Admin panel.");
+		$session_user->message("You are logged in as User. Can not go to Admin panel.");
 		redirect_to("../user/index.php");
 	} else if ($session_restaurant->is_logged_in() ){
 		$session_restaurant->message("You are logged in as Restaurant. Can not go to admin panel.");
