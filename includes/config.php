a07bd0c0d00775fa0abd02fdc823ec03527f4e6d
Grzegorz Lasak <31613060+bluznierca1@users.noreply.github.com>
2018-07-14 18:51:43 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 	!defined("DB_SERVER") ? define("DB_SERVER", "localhost") : null;
 	!defined("DB_USER") ? define("DB_USER", "root") : null;
-	!defined("DB_PASS") ? define("DB_PASS", "newpass") : null;
+	!defined("DB_PASS") ? define("DB_PASS", "**********") : null;
 	!defined("DB_NAME") ? define("DB_NAME", "getorder") : null;	
 
 ?>
